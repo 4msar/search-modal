1ced6cc038d2b380121382c9650e895ff8f34ef1
Saiful Alam <msa4rakib@gmail.com>
2022-09-21 09:44:54 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Search Modal
+
+This is a simple search modal that can be used in any project. It is built with vanilla JavaScript and CSS.
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var targets = [
+  {
+    target: 'web',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'index.min.js',
+      libraryTarget: 'var',
+      library: 'Search Modal'
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({ minimize: true })
+    ],
+  },
+  {
+    target: 'node',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'index.js',
+      libraryTarget: 'commonjs2'
+    }
+  }
+];
+
+var baseConfig = {
+  entry: './index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'lib'),
+        loaders: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  }
+};
+
+targets.forEach(function (target) {
+  var config = Object.assign({}, baseConfig, target);
+
+  webpack(config).run(function (err, stats) {
+    console.log('Generating minified bundle for production use via Webpack...');
+
+    if (err) {
+      console.log(err);
+      return 1;
+    }
+
+    var jsonStats = stats.toJson();
+
+    if (jsonStats.hasErrors) return jsonStats.errors.map(function (error) { return console.log(error); });
+
+    if (jsonStats.hasWarnings) {
+      console.log('Webpack generated the following warnings: ');
+      jsonStats.warnings.map(function (warning) { return console.log(warning); });
+    }
+
+    console.log('Webpack stats: ' + stats.toString());
+
+    //if we got this far, the build succeeded.
+    console.log('Package has been compiled into /dist.');
+    return 0;
+  });
+});
